9799cd9f56cdad7cc6079c0e77599a7ff09217ec
mikevar <96454620+mikevar@users.noreply.github.com>
2023-01-31 22:20:15 +0700

    Installing composer base packages

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'mikevar/standard-monopoly-cli',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mikevar/standard-monopoly-cli' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
